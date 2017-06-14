06a91e2de8a65a10495f68d8f6d36c704bc07be2
pekoe09 <juha.kangas@welho.com>
2017-06-14 16:26:38 +0300

    Started out adding persistence structure; first test (riding on Sql Server 2012 test db) works.

---
+++ Vallila.Domain.Tests/InMemoryDatabaseForXmlMappings.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using NHibernate;
+using NHibernate.Cfg;
+using NHibernate.Dialect;
+using NHibernate.Driver;
+using NHibernate.Tool.hbm2ddl;
+using Environment = NHibernate.Cfg.Environment;
+
+namespace Vallila.Domain.Tests
+{
+    public class InMemoryDatabaseForXmlMappings : IDisposable
+    {
+        protected Configuration config;
+        protected ISessionFactory sessionFactory;
+
+        public InMemoryDatabaseForXmlMappings()
+        {
+            config = new Configuration()
+                .SetProperty(Environment.ReleaseConnections, "on_close")
+                .SetProperty(Environment.Dialect, typeof(MsSql2012Dialect).AssemblyQualifiedName)
+                .SetProperty(Environment.ConnectionDriver, typeof(SqlClientDriver).AssemblyQualifiedName)
+                .SetProperty(Environment.ConnectionString, @"data source=.\SQLEXPRESS;database=vallilatst;integrated security=true")
+                .AddFile(@"C:\Koodausprojektit\vallila\Vallila.Persistence\Mappings\Xml\Activity.hbm.xml");
+            sessionFactory = config.BuildSessionFactory();
+            Session = sessionFactory.OpenSession();
+            new SchemaExport(config).Execute(true, true, false, Session.Connection, Console.Out);
+        }
+
+        public ISession Session { get; set; }
+
+        public void Dispose()
+        {
+            Session.Dispose();
+            sessionFactory.Dispose();
+        }
+    }
+}
